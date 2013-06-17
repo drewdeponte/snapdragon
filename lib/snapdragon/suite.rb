e409feef79fc4ec96d29d46cd9ccc3423595fa64
Andrew De Ponte <cyphactor@gmail.com>
2013-06-17 16:01:10 -0700

    Reworked Suite to store require_paths.
    I did this so the suite as a whole can have a unique set of necessary require
    paths. This will allow us to add logic to handle eliminating duplicate require
    file content.

--- lib/snapdragon/suite.rb
+++ lib/snapdragon/suite.rb
@@ -2,14 +2,18 @@ module Snapdragon
   class Suite
     def initialize
       @specs = []
+      @require_paths = []
     end
 
     def add_spec_file(spec_file)
       @specs << spec_file
+      @require_paths.concat(spec_file.require_paths)
     end
 
     def add_spec_files(spec_files)
-      @specs.concat(spec_files)
+      spec_files.each do |spec|
+        add_spec_file(spec)
+      end
     end
 
     def spec_files
--- spec/lib/snapdragon/suite_spec.rb
+++ spec/lib/snapdragon/suite_spec.rb
@@ -6,11 +6,16 @@ describe Snapdragon::Suite do
       suite = Snapdragon::Suite.new
       suite.instance_variable_get(:@specs).should eq([])
     end
+
+    it "assigns an empty arary to hold all the require files" do
+      suite = Snapdragon::Suite.new
+      suite.instance_variable_get(:@require_paths).should eq([])
+    end
   end
 
-  describe "#specs" do
+  describe "#spec_files" do
     it "returns the array of specs which the suite contains" do
-      spec_file = stub
+      spec_file = stub(:require_paths => [])
       subject.add_spec_file(spec_file)
       subject.spec_files.should eq([spec_file])
     end
@@ -18,21 +23,37 @@ describe Snapdragon::Suite do
 
   describe "#add_spec_file" do
     it "appends the given SpecFile to the suite of specs" do
-      spec_file = stub
+      spec_file = stub(:require_paths => [])
       specs = mock
       subject.instance_variable_set(:@specs, specs)
       specs.should_receive(:<<).with(spec_file)
       subject.add_spec_file(spec_file)
     end
+
+    it "gets the require paths outlined in the spec" do
+      spec_file = mock
+      spec_file.should_receive(:require_paths).and_return([])
+      subject.add_spec_file(spec_file)
+    end
+
+    it "contactinates the specs require paths to the suites require paths collection" do
+      spec_file = stub
+      require_paths = stub
+      suite_require_paths = mock
+      subject.instance_variable_set(:@require_paths, suite_require_paths)
+      spec_file.stub(:require_paths).and_return(require_paths)
+      suite_require_paths.should_receive(:concat).with(require_paths)
+      subject.add_spec_file(spec_file)
+    end
   end
 
   describe "#add_spec_files" do
-    it "concatinates the given array of SpecFiles to the suite of specs" do
-      spec_files = stub
-      specs = mock
-      subject.instance_variable_set(:@specs, specs)
-      specs.should_receive(:concat).with(spec_files)
-      subject.add_spec_files(spec_files)
+    it "adds each of the spec files" do
+      spec_file_one = stub('spec_file_one')
+      spec_file_two = stub('spec_file_two')
+      subject.should_receive(:add_spec_file).with(spec_file_one)
+      subject.should_receive(:add_spec_file).with(spec_file_two)
+      subject.add_spec_files([spec_file_one, spec_file_two])
     end
   end
 end
