618f2758d94ea7c909efdc5b335c4da4be957acc
Andrew De Ponte <cyphactor@gmail.com>
2013-06-17 18:02:48 -0700

    Made it work when given a directory or directories.

--- lib/snapdragon/spec_directory.rb
+++ lib/snapdragon/spec_directory.rb
@@ -1,7 +1,19 @@
+require_relative './spec_file'
+
 module Snapdragon
   class SpecDirectory
     def initialize(path)
       @path = path
+    end
+
+    def spec_files
+      spec_paths = []
+      Dir.glob("#{@path}/**/*").each do |path|
+        if path =~ /^[\w\/\-\d]+[s|S]pec\.js$/
+          spec_paths << Snapdragon::SpecFile.new(path)
+        end
+      end
+      return spec_paths
     end
   end
 end
