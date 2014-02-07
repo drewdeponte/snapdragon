a1732cdbd80f42c1625e51d41a8cc0fb30c0b7f2
Jack Wei <jack.wei@reachlocal.com>
2014-02-07 10:29:48 -0800

    Update gems to the latest version.
    Update gems to the latest version.
    Update Rspec tests to be compatible with Rspec 2.x.
    Disable "should" syntax.

--- spec/lib/snapdragon/cli_application_spec.rb
+++ spec/lib/snapdragon/cli_application_spec.rb
@@ -1,54 +1,55 @@
+require 'spec_helper'
 require_relative '../../../lib/snapdragon/cli_application'
 
 describe Snapdragon::CliApplication do
   describe "#initialize" do
     it "creates an empty Suite" do
-      Snapdragon::Suite.should_receive(:new)
-      Snapdragon::CliApplication.new(stub, stub)
+      expect(Snapdragon::Suite).to receive(:new)
+      Snapdragon::CliApplication.new(double, double)
     end
 
     it "assigns the new Suite to an instance variable" do
-      suite = stub('suite')
-      Snapdragon::Suite.stub(:new).and_return(suite)
-      app = Snapdragon::CliApplication.new(stub, stub)
-      app.instance_variable_get(:@suite).should eq(suite)
+      suite = double('suite')
+      allow(Snapdragon::Suite).to receive(:new).and_return(suite)
+      app = Snapdragon::CliApplication.new(double, double)
+      expect(app.instance_variable_get(:@suite)).to be(suite)
     end
   end
 
   describe "#run" do
-    let(:paths) { stub('paths') }
-    let(:options) { stub('options') }
+    let(:paths) { double('paths') }
+    let(:options) { double('options') }
     subject { Snapdragon::CliApplication.new(options, paths) }
 
     it "creates a capybara session" do
-      suite = stub(filtered?: false)
-      Snapdragon::Suite.stub(:new).and_return(suite)
-      Capybara::Session.should_receive(:new).and_return(stub.as_null_object)
+      suite = double(filtered?: false)
+      allow(Snapdragon::Suite).to receive(:new).and_return(suite)
+      expect(Capybara::Session).to receive(:new).and_return(double.as_null_object)
       subject.run
     end
 
     context "when suite is filtered" do
       before do
-        subject.instance_variable_set(:@suite, stub(:filtered? => true, :spec_query_param => 'some_query_param_spec_filter'))
+        subject.instance_variable_set(:@suite, double(:filtered? => true, :spec_query_param => 'some_query_param_spec_filter'))
       end
 
       it "visits /run with the spec query param in the capybara session" do
-        session = mock(find: nil)
-        Capybara::Session.stub(:new).and_return(session)
-        session.should_receive(:visit).with("/run?spec=some_query_param_spec_filter")
+        session = double(find: nil)
+        allow(Capybara::Session).to receive(:new).and_return(session)
+        expect(session).to receive(:visit).with("/run?spec=some_query_param_spec_filter")
         subject.run
       end
     end
 
     context "when suite is NOT filtered" do
       before do
-        subject.instance_variable_set(:@suite, stub(:filtered? => false))
+        subject.instance_variable_set(:@suite, double(:filtered? => false))
       end
 
       it "visits /run in that capybara session" do
-        session = mock(find: nil)
-        Capybara::Session.stub(:new).and_return(session)
-        session.should_receive(:visit).with('/run')
+        session = double(find: nil)
+        allow(Capybara::Session).to receive(:new).and_return(session)
+        expect(session).to receive(:visit).with('/run')
         subject.run
       end
     end
