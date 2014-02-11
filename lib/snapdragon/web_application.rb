569991083f407fa2852d3ebadaf6669fce21e670
Jack Wei <jack.wei@reachlocal.com>
2014-02-11 13:10:17 -0800

    Add Jasmine v2 as the default framework.
    - Add Jasmine v2 as the default framework.
    - Keep Jasmine v1.3.1 for backward compatibility since v2 does not suppor IE<8.
    - Updated color/no-color arg switch.

--- lib/snapdragon/web_application.rb
+++ lib/snapdragon/web_application.rb
@@ -12,7 +12,15 @@ module Snapdragon
     end
 
     get "/run" do
-      erb :run
+      if @suite.jasmine_ver == "1"
+        erb :run_v1
+      else
+        erb :run
+      end
+    end
+
+    get "/jasmine/*" do |path|
+      send_file ::File.expand_path(::File.join('../jasmine', path), ::File.dirname(__FILE__))
     end
 
     get "/jasmine_v1/*" do |path|
